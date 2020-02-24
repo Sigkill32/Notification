98cc8b94b35b56beeeb984c3e748b47ceb77908b
Manoj Bhat <bhatanm@gmail.com>
2020-02-24 11:14:04 +0530

    Converted Notify to functional component

--- src/App.js
+++ src/App.js
@@ -10,21 +10,12 @@ class App extends Component {
       title: "Title",
       description: "Some description",
       type: "notification"
-      // width: 400
     });
   };
 
   render() {
     return (
       <div style={{ marginLeft: "20px" }}>
-        <h1>Hello world</h1>
-        {/* <Notify
-          description='This is the content of the notification. This is the content of the notification. This is the content of the notification.'
-          title='Notification Title'
-          type='notification'
-          subType='success'
-          width={400}
-        /> */}
         <button onClick={this.handleClick}>open</button>
       </div>
     );
--- src/components/Notify.js
+++ src/components/Notify.js
@@ -1,24 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import close from "../icons/close.svg";
 
-class Notify extends Component {
-  state = {};
-
-  renderNotificationIcon = () => {
-    const { subType } = this.props;
+const Notify = ({
+  description,
+  type,
+  closeIcon,
+  width,
+  id,
+  className,
+  onClick,
+  onClose,
+  title,
+  position,
+  subType
+}) => {
+  const renderNotificationIcon = () => {
     const subTypes = ["error", "info", "success", "warn"];
+    const src = require(`../icons/${subType}.svg`);
     if (subTypes.includes(subType)) {
       return (
         <div className='notification-icon'>
-          <img src={require(`../icons/${subType}.svg`)} alt={subType} />
+          <img src={src} alt={subType} />
         </div>
       );
     } else return null;
   };
 
-  setPosition = () => {
-    const { position } = this.props;
-
+  const setPosition = () => {
     switch (position) {
       case "topLeft":
         return { top: "20px", left: "20px" };
@@ -33,8 +41,7 @@ class Notify extends Component {
     }
   };
 
-  renderTitle = () => {
-    const { type, title } = this.props;
+  const renderTitle = () => {
     if (type === "notification") {
       if (typeof title === "string")
         return <div className='notification-title'>{title}</div>;
@@ -43,53 +50,45 @@ class Notify extends Component {
     if (type === "message") return null;
   };
 
-  handleClose = e => {
-    const { onClose } = this.props;
+  const handleClose = e => {
     const { id } = e.target;
-    this.UnMountNotification(id);
+    UnMountNotification(id);
     // onClose();
   };
 
-  handleClick = () => {
-    const { onClick } = this.props;
+  const handleClick = () => {
+    console.log("click");
     // onClick();
   };
 
-  UnMountNotification = id => {
+  const UnMountNotification = id => {
     const element = document.querySelector(`#app-notification-${id}`);
     if (element) element.parentNode.removeChild(element);
   };
 
-  render() {
-    const { description, type, closeIcon, width, id, className } = this.props;
-    return (
-      <div
-        className={`notification-container${className ? " " + className : ""}`}
-        style={{ width: `${width}px`, ...this.setPosition() }}
-        onClick={this.handleClick}
-        id={id}
-      >
-        {this.renderNotificationIcon()}
-        <div className='notification'>
-          {this.renderTitle()}
-          {typeof description === "string" ? (
-            <div className='notification-desc'>{description}</div>
-          ) : (
-            description
-          )}
-        </div>
-        {type === "notification" && (
-          <button
-            className='close-notification'
-            onClick={this.handleClose}
-            id={id}
-          >
-            <img src={closeIcon ? closeIcon : close} alt='close' id={id} />
-          </button>
+  return (
+    <div
+      className={`notification-container${className ? " " + className : ""}`}
+      style={{ width: `${width}px`, ...setPosition() }}
+      onClick={handleClick}
+      id={id}
+    >
+      {renderNotificationIcon()}
+      <div className='notification'>
+        {renderTitle()}
+        {typeof description === "string" ? (
+          <div className='notification-desc'>{description}</div>
+        ) : (
+          description
         )}
       </div>
-    );
-  }
-}
+      {type === "notification" && (
+        <button className='close-notification' onClick={handleClose} id={id}>
+          <img src={closeIcon ? closeIcon : close} alt='close' id={id} />
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default Notify;
